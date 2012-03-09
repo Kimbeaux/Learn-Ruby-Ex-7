8680dec95765d9f75db77f5ae835ed48c2015028
Kimbeaux <KimbeauxE@verizon.net>
2012-03-09 15:38:47 -0500

    ex 7 w/ all ec

---
+++ ex7.rb
@@ -0,0 +1,42 @@
+# print string
+puts "Mary had a little lamb."
+# print string w/ string 'snow'
+puts "Its fleece was white as %s." % 'snow'
+# print string
+puts "And everywhere that Mary went." 
+# print 10 periods
+puts "." * 10  # what'd that do?
+
+# string C in variable end1
+end1 = "C"
+# string h in variable end2
+end2 = "h"
+# string e in variable end3
+end3 = "e"
+# string e in variable end4
+end4 = "e"
+# string s in variable end5
+end5 = "s"
+# string e in variable end6
+end6 = "e"
+# string B in variable end7
+end7 = "B"
+# string u in variable end8
+end8 = "u"
+# string r in variable end9
+end9 = "r"
+# string g in variable end10
+end10 = "g"
+# string e in variable end11
+end11 = "e"
+# string r in variable end12
+end12 = "r"
+
+# notice how we are using print instead of puts here.  change it to puts
+# and see what happens.
+# puts adds a carriage return at the end of the line
+puts end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+
+# this just is polite use of the terminal, try removing it
+# puts
